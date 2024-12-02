16b60885d6ee4014958a3aee72d212b2f54c0bd8
pakhrom <ro.pakhomov@gmail.com>
2024-12-02 21:18:48 +0700

    Remove already done TODO

--- Assets/Code/Scripts/BulletMovement.cs
+++ Assets/Code/Scripts/BulletMovement.cs
@@ -11,7 +11,6 @@ namespace Code.Scripts
         private void Update()
         {
             transform.position += transform.right * (_speed * Time.deltaTime);
-            // TODO: Implement Destroy method when outside the camera's viewport
         }
 
         private void OnTriggerExit2D(Collider2D other)
