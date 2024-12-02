87acf5dd877ec5fd2aa49a4cf9f9f726d1e184d2
pakhrom <ro.pakhomov@gmail.com>
2024-12-02 21:19:02 +0700

    Rewrite GroundCheck

--- Assets/Code/Scripts/GroundCheck.cs
+++ Assets/Code/Scripts/GroundCheck.cs
@@ -1,22 +1,36 @@
+using System;
 using UnityEngine;
 
 namespace Code.Scripts
 {
     public class GroundCheck : MonoBehaviour
     {
-        [SerializeField] private LayerMask _layer;
-        [SerializeField] private float _threshold;
+        // [SerializeField] private LayerMask _layer;
+        // [SerializeField] private float _threshold;
 
-        private Collider2D _collider;
+        public bool isGrounded;
+        
+        // private Collider2D _collider;
 
-        private void Awake()
-        {
-            _collider = GetComponent<Collider2D>(); 
-        }
+        // private void Awake()
+        // {
+            // _collider = GetComponent<Collider2D>(); 
+        // }
         public bool IsGrounded()
         {
-            RaycastHit2D boxCast = Physics2D.BoxCast(_collider.bounds.center, _collider.bounds.size * 1,0f,Vector2.down,  _threshold, _layer);
-            return boxCast.collider;
+            // RaycastHit2D boxCast = Physics2D.BoxCast(_collider.bounds.center, _collider.bounds.size * 1,0f,Vector2.down,  _threshold, _layer);
+            // return boxCast.collider;
+            return isGrounded;
+        }
+
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            isGrounded = true;
+        }
+
+        private void OnTriggerExit2D(Collider2D other)
+        {
+            isGrounded = false;
         }
     }
 }
